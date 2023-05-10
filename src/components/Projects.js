9b2d5a146ec2a4fa49f03714a91b868219974252
Rafiul29 <rafiulislam4472@gmail.com>
2023-05-10 18:02:39 +0600

    add skills and new projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,8 +14,24 @@ const data = [
     liveLink: "https://rafiul-tech-alpha.netlify.app/",
     fontEndLink: "https://github.com/Rafiul29/tech-alpha.git",
   },
+ 
   {
     id: 2,
+    title:
+      "Proxima | Project Management App",
+    img1: "https://res.cloudinary.com/dgjjgijyu/image/upload/c_limit,f_jpg,fl_lossy.any_format.preserve_transparency.progressive,h_1600,pg_1,q_auto,w_1600/v1/portfolio-project/proxima-1",
+    img2: "https://res.cloudinary.com/dgjjgijyu/image/upload/c_limit,f_jpg,fl_lossy.any_format.preserve_transparency.progressive,h_1600,pg_1,q_auto,w_1600/v1/portfolio-project/proxima-2",
+    describe:
+      "Pormixa is an innovative project management app that helps users manage their projects with ease. With its powerful CRUD (Create, Read, Update, Delete) operations and intuitive interface, Pormixa allows users to create, update, and delete projects effortlessly. Pormixa uses JWT authentication, which allows only authorized users to access the app's features. ",
+    tools: ["React","Express","Mongodb","Context-api","Tailwind CSS", ],
+    liveLink: "https://rafiul-proxima.netlify.app/",
+    fontEndLink: "https://github.com/Rafiul29/proxima-client.git",
+    backEndLink: "https://github.com/Rafiul29/proxima-server.git",
+   
+  },
+
+  {
+    id: 3,
     title:
       "Cyberbullying-Cyber | Stand Up Against Cyberbullying.",
     img1: "https://res.cloudinary.com/dgjjgijyu/image/upload/v1675864972/portfolio-project/Cyberbulling-1_h9kqmx.png",
